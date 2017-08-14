c23e66cdcb224e7d117d41311a4df14426b63a8f
Travis Payne <thpaggie910@gmail.com>
2017-08-14 16:48:36 -0500

    Complete user interaction portion of Internal and External JavaScript Exercise

--- js/external.js
+++ js/external.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+var favoriteColor = prompt("What is your favorite color?");
+
+alert("Great, " + favoriteColor + " is my favorite color, too!");
