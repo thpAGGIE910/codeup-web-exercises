3cd6d065d8598315c20d5fd26e2f712ec92552f8
Travis Payne <thpaggie910@gmail.com>
2017-08-15 11:24:23 -0500

    Refactor code from if-else.js to use a switch in switch.js

--- js/switch.js
+++ js/switch.js
@@ -11,6 +11,29 @@ switch (color) {
     // TODO: create a case statement that will handle every color except indigo and violet
     // TODO: when a color is encountered log a message that tells the color, and an object of that color
     //       example: Blue is the color of the sky.
+    case 'red':
+        console.log('Fire engines are red');
+        break;
+
+    case 'orange':
+        console.log('Pumpkins are orange');
+        break;
+
+    case 'yellow':
+        console.log('Canaries is yellow');
+        break;
+
+    case 'green':
+        console.log('The grass is green');
+        break;
+
+    case 'blue':
+        console.log('The sky is blue');
+        break;
+
+    default:
+        console.log('I\'m sorry but I don\'t know that color...');
+        break;
 
     // TODO: create a default case that will catch indigo and violet
     // TODO: for the default case, log: I do not know anything by that color.
