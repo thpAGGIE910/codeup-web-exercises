becb2d5c8cc3a067346636fb53f53ba63ae9180d
Travis Payne <thpaggie910@gmail.com>
2017-08-16 15:33:44 -0500

    Add solution for the Random Numbers exercise to for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,3 +8,19 @@ alert("Check the console for the multiplication table for " + userNumber);
 for (var i = 1; i <= 10; i++) {
     console.log(userNumber + " * " + i + " = " + userNumber * i);
 }
+
+// ----- Random Numbers -----
+const MIN_NUMBER = 20;
+const MAX_NUMBER = 200;
+
+var randomNumber = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER)) + MIN_NUMBER;
+
+for(var i = 0; i < 10; i++) {
+    if((randomNumber % 2) == 0) {
+        console.log(randomNumber + " is even.");
+    } else {
+        console.log(randomNumber + " is odd.");
+    }
+
+    randomNumber = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER)) + MIN_NUMBER;
+}
