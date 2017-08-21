fb5a3010fda7e047598cf6d44685d2e8b0171b39
Travis Payne <thpaggie910@gmail.com>
2017-08-21 14:02:24 -0500

    Add first two extra credit exercises to planets-string.js

--- js/planets-string.js
+++ js/planets-string.js
@@ -21,4 +21,40 @@
     //        console.log() your results.
     var ul = "<ul><li>" + planetsString.split("|").join("</li><li>") + "</li></ul>";
     console.log(ul);
+
+    /*
+    * Extra Credit
+    * https://gist.github.com/zgulde/6caa27c302cf5a28b534b7f3f375e03a
+    *
+    * Write a function that accepts an array whose elements are arrays where each inner array consists of two numbers.
+    * The function should return the index of the element whose two inner numbers add up to 9, or -1 if no pairs add up
+    * to 9.
+    *
+    * findIndexWhereSumIs9([[1, 2], [3, 4], [4, 5], [9, 1]]) // returns 2
+    * findIndexWhereSumIs9([[10, 2], [11, -2], [2, 5], [9, 1]]) // returns 1
+    * findIndexWhereSumIs9([[10, 2], [11, -7], [2, 5], [9, 1]]) // returns -1
+    */
+    function findIndexWhereSumIs9(arrays) {
+        return sumEmUp(arrays).indexOf(9);
+    }
+
+    /*
+     * Write a function that accepts an array whose elements are arrays of numbers. The function should return the
+     * original array, but in the place of each element, the sum of the numbers from the inner array.
+     *
+     * sumEmUp([[1], [1, 2, 3], [5, 5, 5], [1, 1, 1, 1, 1]]) // returns [1, 6, 15, 5]
+     */
+    function sumEmUp(arrays) {
+        return arrays.map(function (array) {
+            return array.reduce(function(sum, value) {
+                return sum + value;
+            }, 0);
+        });
+    }
+
+    console.log(findIndexWhereSumIs9([[1, 2], [3, 4], [4, 5], [9, 1]]));
+    console.log(findIndexWhereSumIs9([[10, 2], [11, -2], [2, 5], [9, 1]]));
+    console.log(findIndexWhereSumIs9([[10, 2], [11, -7], [2, 5], [9, 1]]));
+
+    console.log(sumEmUp([[1], [1, 2, 3], [5, 5, 5], [1, 1, 1, 1, 1]]));
 })();
