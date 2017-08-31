1dd8dd0a0bc576ea7eb4d5e0f2609169b4b3bc6f
Travis Payne <thpaggie910@gmail.com>
2017-08-31 11:28:12 -0500

    Finish 'Multiple Selectors' section of the exercise

--- js/jquery-selectors.js
+++ js/jquery-selectors.js
@@ -13,12 +13,13 @@
         // $(".codeup:nth-child(3)").css("border", "none");
 
         // Element Selectors Exercises
-        $("li").css("font-size", "20px");
-        $("h1").css("background-color", "yellow");
-        $("p").css("background-color", "yellow");
-        $("li").css("background-color", "yellow");
-        alert($('h1').html());
+        // $("li").css("font-size", "20px");
+        // $("h1").css("background-color", "yellow");
+        // $("p").css("background-color", "yellow");
+        // $("li").css("background-color", "yellow");
+        // alert($('h1').html());
 
-
+        // Multiple Selectors Exercise
+        $("li, h1, p").css("background-color", "yellow");
     });
 })();
