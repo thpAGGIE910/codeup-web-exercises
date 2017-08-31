061d00c893aa1fa40af9ebbcebcbd4d744b3bb41
Travis Payne <thpaggie910@gmail.com>
2017-08-31 11:10:52 -0500

    Finish 'Class Selectors' section of the exercise

---
+++ js/jquery-selectors.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+
+    $(document).ready(function () {
+        // alert($("#article-1-title").html());
+        // alert($("#username").html());
+        // var $articles = $("#article-1");
+        // console.log($articles);
+
+        $(".codeup").css("border", "1px solid red");
+        $(".codeup:nth-child(3)").css("border", "none");
+
+
+    });
+})();
