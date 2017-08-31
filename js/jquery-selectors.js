7fbc455468c010c8981d267061c293d597c1c1b7
Travis Payne <thpaggie910@gmail.com>
2017-08-31 11:26:04 -0500

    Finish 'Element Selctors' section of the exercise

--- js/jquery-selectors.js
+++ js/jquery-selectors.js
@@ -2,13 +2,22 @@
     "use strict";
 
     $(document).ready(function () {
+        // ID Selector Exercises
         // alert($("#article-1-title").html());
         // alert($("#username").html());
         // var $articles = $("#article-1");
         // console.log($articles);
 
-        $(".codeup").css("border", "1px solid red");
-        $(".codeup:nth-child(3)").css("border", "none");
+        // Class Selector Exercises
+        // $(".codeup").css("border", "1px solid red");
+        // $(".codeup:nth-child(3)").css("border", "none");
+
+        // Element Selectors Exercises
+        $("li").css("font-size", "20px");
+        $("h1").css("background-color", "yellow");
+        $("p").css("background-color", "yellow");
+        $("li").css("background-color", "yellow");
+        alert($('h1').html());
 
 
     });
